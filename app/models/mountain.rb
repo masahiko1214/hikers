102efc3162c7e06cf7cd0c176709d846ebb90fda
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-03-14 17:19:47 +0900

    テーブル設計

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,2 +1,5 @@
 class Mountain < ApplicationRecord
+
+  def  index
+  end
 end
--- db/migrate/20210313155933_create_mountains.rb
+++ db/migrate/20210313155933_create_mountains.rb
@@ -1,7 +1,12 @@
 class CreateMountains < ActiveRecord::Migration[6.0]
   def change
     create_table :mountains do |t|
-
+      t.integer :elevation, null: false
+      t.integer :level_id, null: false
+      t.integer :time_id, null: false
+      t.integer :prefectures_id, null: false
+      t.string :municipality, null: false
+      t.string :address, null: false
       t.timestamps
     end
   end

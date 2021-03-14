48d6db40f1c0902c99c893902f271b51dced34c6
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-03-14 21:07:34 +0900

    フロント調整

--- db/migrate/20210313155933_create_mountains.rb
+++ db/migrate/20210313155933_create_mountains.rb
@@ -1,6 +1,8 @@
 class CreateMountains < ActiveRecord::Migration[6.0]
   def change
     create_table :mountains do |t|
+      t.string :name,null: false
+      t.text :explanation, null: false
       t.integer :elevation, null: false
       t.integer :level_id, null: false
       t.integer :time_id, null: false

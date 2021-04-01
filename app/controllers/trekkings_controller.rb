5051c5fd71ea0a8a1817adae0d7bc7ddad8559ee
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-02 01:23:31 +0900

    saveの調整

--- app/controllers/trekkings_controller.rb
+++ app/controllers/trekkings_controller.rb
@@ -5,13 +5,20 @@ class TrekkingsController < ApplicationController
   def index
     @mountain = Mountain.find(params[:mountain_id])
     @trekking = Trekking.new(hiker_id: current_hiker.id,mountain_id: params[:mountain_id])
-    @trekking.save
+
+    unless Trekking.exists?(hiker_id: current_hiker.id,mountain_id: params[:mountain_id])  # カラムにhiker_id と mountain_idがなければ保存する
+      @trekking.save
+    end
+
+
+    
     
     
     
   end
 
 def new
+  @trekking = Trekking.find_by(hiker_id: current_hiker.id,mountain_id: params[:mountain_id]) # 現在ログインしているhiker_idとmountain_idを探索している
   @mountain = Mountain.find(params[:mountain_id])
     
 
--- app/models/trekking.rb
+++ app/models/trekking.rb
@@ -2,4 +2,115 @@ class Trekking < ApplicationRecord
   belongs_to :hiker
   belongs_to :mountain
 
+  def calorie (weight, level, time)
+  
+    if  level == 2
+
+      if time == 2
+       weight*6*0.5
+
+      elsif time == 3
+        weight*6*1
+
+      elsif time == 4
+        weight*6*1.5
+
+      elsif time == 5
+        weight*6*2.5
+
+      elsif time == 6
+        weight*6*3.5
+
+      elsif time == 7
+        weight*6*4.5
+
+      elsif time == 8
+        weight*6*5.5
+
+      elsif time == 9
+        weight*6*6.5
+
+      elsif time == 10
+        weight*6*7
+      end
+
+    elsif level == 3
+
+      if time == 2
+        weight*7*0.5
+ 
+        elsif time == 3
+         weight*7*1
+ 
+        elsif time == 4
+         weight*7*1.5
+ 
+        elsif time == 5
+         weight*7*2.5
+ 
+        elsif time == 6
+         weight*7*3.5
+ 
+        elsif time == 7
+         weight*7*4.5
+ 
+        elsif time == 8
+         weight*7*5.5
+ 
+        elsif time == 9
+         weight*7*6.5
+ 
+        elsif time == 10
+         weight*7*7
+       end
+ 
+      elsif level == 4
+
+        if time == 2
+          weight*8*0.5
+   
+        elsif time == 3
+           weight*8*1
+   
+          elsif time == 4
+           weight*8*1.5
+   
+          elsif time == 5
+           weight*8*2.5
+   
+          elsif time == 6
+           weight*8*3.5
+   
+          elsif time == 7
+           weight*8*4.5
+   
+          elsif time == 8
+           weight*8*5.5
+   
+          elsif time == 9
+           weight*8*6.5
+   
+          elsif time == 10
+           weight*8*7
+         end
+
+
+
+
+
+
+
+
+
+    end
+
+
+
+
+
+
+
+
+    end
+
 end
